2e8e85660abe2b05c610762ac20f57ab769c1115
Abhijeet Vashistha <abhivashistha5@gmail.com>
2024-03-19 23:23:56 +0530

    Basic password generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use rand::Rng;
+
 fn main() {
-    println!("Access Granted!");
+    // Initialize the characters that are valid for the passwords
+    let valid_chars_string =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-!@#$%&(){};?";
+    let mut valid_chars: Vec<String> = Vec::new();
+    for chars in valid_chars_string.chars() {
+        valid_chars.push(chars.to_string());
+    }
+
+    const PASSWORD_LENGTH: usize = 10;
+
+    // Randomly select characters
+    let mut rng = rand::thread_rng();
+    let mut password: Vec<String> = Vec::new();
+    let mut random_index: usize;
+    for _ in 0..PASSWORD_LENGTH {
+        random_index = rng.gen_range(0..valid_chars.len());
+        password.push(valid_chars[random_index].clone());
+    }
+
+    println!("{}", password.join(""));
 }
